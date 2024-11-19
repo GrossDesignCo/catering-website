4381a6333905e4e101dd974fc479b2fee2cce26c
Matt Gross <mattgrosswork@gmail.com>
2024-11-19 15:39:15 -0800

    add noindex to prevent premature search indexing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* TODO: Remove before official launch */}
+        <meta name="robots" content="noindex" />
+      </Head>
       <body>
         <Main />
         <NextScript />
